da44787011731bf88b450eae8ddd9d141c444433
kishore rawat <kishore.rawat@gmail.com>
2016-05-18 17:19:39 +0530

    Some minor formatting corrections

--- plot1.R
+++ plot1.R
@@ -15,18 +15,23 @@ source("getdata.R")
 ## summary of the data using group_by and summarize functions of dplyr library
 ## load the dplyr package
 library(dplyr)
+
 ## Group the data yearwise first
 Yrwise_Grp <- group_by(NEI, year)
 ## Now, create a summary data, where the sum of "Emissions" is done for each year
 Yrwise_Total_Emissions <- summarize(Yrwise_Grp, sum(Emissions))
+
 ## Give meaningful column names 
 names(Yrwise_Total_Emissions) <- c("Year", "Tot_Emissions")
+
 ## before plotting, set the device to a PNG file
 png("plot1.png", width=480, height=480, bg="transparent")
+
 ## plot the line graph showing a clear downtrend in the PM2.5 emissions from year
 ## 1999 to 2008
 plot(Yrwise_Total_Emissions, xlab="Year", ylab="Total PM2.5 Emissions", 
      main="Total PM2.5 Emissions in US - Years 1999-2008", 
      type="b", lwd=3, pch=9, col="blue", cex=1, col.lab="brown")
+
 ## close the PNG file device
 dev.off()
